11e63f8fc4c4892cc7aeaf959d6757aae5832431
AlonsoDiCandia <alonsodiazcandia@gmail.com>
2022-10-29 22:21:55 -0300

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Alonso Diaz Candia on 29-10-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/Home_PanelApp.swift
@@ -0,0 +1,17 @@
+//
+//  Home_PanelApp.swift
+//  Shared
+//
+//  Created by Alonso Diaz Candia on 29-10-22.
+//
+
+import SwiftUI
+
+@main
+struct Home_PanelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
